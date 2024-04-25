9b9d838345acf0cd1dd6b8084f55cd28f0bd0968
LucasBondeDeSouza <bondelucas77@gmail.com>
2024-04-25 15:59:39 -0300

    navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,35 @@
+import { useState, useEffect } from "react"
+import MovieCard from "../components/MovieCard"
+
+const moviesUrl = import.meta.env.VITE_API
+const apiKey = import.meta.env.VITE_API_KEY
+
 const Home = () => {
+  const [topMovies, setTopMovies] = useState([])
+
+  const getTopRatedMovies = async (url) => {
+
+    const res = await fetch(url)
+    const data = await res.json()
+
+    setTopMovies(data.results)
+  }
+
+  useEffect(() => {
+
+    const topRateUrl = `${moviesUrl}top_rated?${apiKey}`
+
+    getTopRatedMovies(topRateUrl)
+
+  }, [])
+
   return (
-    <div>
-      Home
+    <div className="container">
+      <h2 className="title">Melhores filmes:</h2>
+      <div className="movies-container">
+        {topMovies.length == 0 && <p>Carregando...</p>}
+        {topMovies.length > 0 && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
     </div>
   )
 }
